897942f7aa7c25e1cbfa643b1571a2f9ddeb0c5e
arnaudPortay <arnaud.portay@telecom-st-etienne.fr>
2019-05-29 17:06:25 +0200

    update deploy script for installer creation and add Qt installer framework config files

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,16 @@
+REM Creating bin directory
+mkdir .\Installer_Data\packages\com.apy.mydesktop\bin
 REM Deploy libaries
-windeployqt --no-translations --no-angle --no-webkit2 --release --qmldir . --dir ..\MyDesktop_Package\ ..\MyDesktop-Release\release\MyDesktop.exe
+windeployqt --no-translations --no-angle --no-webkit2 --release --qmldir . --dir .\Installer_Data\packages\com.apy.mydesktop\bin ..\MyDesktop-Release\release\MyDesktop.exe
 REM Copy exe to release path
-robocopy ..\MyDesktop-Release\release\ ..\MyDesktop_Package\ MyDesktop.exe
+robocopy .\Installer_Data\packages\com.apy.mydesktop\bin ..\MyDesktop_Package\ MyDesktop.exe
+REM Zipping data
+archivegen .\Installer_Data\packages\com.apy.mydesktop\data\mydesktop.7z .\Installer_Data\packages\com.apy.mydesktop\bin
+REM Delete unzipped data
+del /q /f .\Installer_Data\packages\com.apy.mydesktop\bin
+REM Delete bin directory
+rmdir /q /s .\Installer_Data\packages\com.apy.mydesktop\bin
+REM Create installer
+binarycreator.exe -f -p .\Installer_Data\packages -c .\Installer_Data\config\config.xml MyDesktopSetup.exe
+REM Delete zipped data
+del /q /f .\Installer_Data\packages\com.apy.mydesktop\data\mydesktop.7z
