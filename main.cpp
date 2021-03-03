45fc2d8050da0a7e14887d918193b7281c46b698
arnaudPortay <arnaud.portay@gmail.com>
2021-03-03 20:15:24 +0100

    Prepare for 1.2.0 release

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     //Uncomment before release.
-//    QApplication::setOrganizationName("MyDesktop");
-//    QApplication::setOrganizationDomain("MyDesktop.com");
-//    QApplication::setApplicationName("MyDesktop");
+    QApplication::setOrganizationName("MyDesktop");
+    QApplication::setOrganizationDomain("MyDesktop.com");
+    QApplication::setApplicationName("MyDesktop");
 
     // Comment before release. Test purposes only
-    QApplication::setOrganizationName("MyDesktop_test ");
-    QApplication::setOrganizationDomain("MyDesktop_test.com");
-    QApplication::setApplicationName("MyDesktop_test");
+//    QApplication::setOrganizationName("MyDesktop_test ");
+//    QApplication::setOrganizationDomain("MyDesktop_test.com");
+//    QApplication::setApplicationName("MyDesktop_test");
 
     QApplication::setApplicationVersion("1.1.1");
 
